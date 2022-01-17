788008271b55a4b4188ba4b3187e28284a030915
Yemiemy <rasholayemi@gmail.com>
2022-01-18 00:47:48 +0100

    multiple users can join

--- main.js
+++ main.js
@@ -8,11 +8,14 @@ let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
+
+    client.on('user-published', handleUserJoined)
+
     let UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
 
-    let player = `<div class="video-container" id="user-container-${UID}">
+    let player = `  <div class="video-container" id="user-container-${UID}">
                         <div class="video-player" id="user-${UID}"></div>
                     </div>`
     document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
@@ -26,6 +29,30 @@ let joinStream = async () => {
     await joinAndDisplayLocalStream()
     document.getElementById('join-btn').style.display = 'none'
     document.getElementById('stream-controls').style.display = 'flex'
+}
+
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user
+    await client.subscribe(user, mediaType)
+
+    if (mediaType === 'video'){
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if (player != null){
+            player.remove()
+        }
+
+        player = `  <div class="video-container" id="user-container-${user.uid}">
+                        <div class="video-player" id="user-${user.uid}"></div>
+                    </div>`
+
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+
+    if (mediaType === 'audio'){
+        user.audioTrack.play()
+    }
 }
 
 document.getElementById('join-btn').addEventListener('click', joinStream)
